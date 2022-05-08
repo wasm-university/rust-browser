8ff449b8c3b830aa49ce316a04784743f54c6f66
Philippe Charrière <ph.charriere@gmail.com>
2022-05-08 06:25:27 +0000

    📝 save

--- 01-hello/hello/target/rls/debug/deps/hello-35e8144458653042.d
+++ 01-hello/hello/target/rls/debug/deps/hello-35e8144458653042.d
@@ -1,5 +1,5 @@
-/workspace/rust-browser/01-hello/hello/target/rls/debug/deps/hello-35e8144458653042.rmeta: src/lib.rs
+/workspace/rust-javascript/01-hello/hello/target/rls/debug/deps/hello-35e8144458653042.rmeta: src/lib.rs
 
-/workspace/rust-browser/01-hello/hello/target/rls/debug/deps/hello-35e8144458653042.d: src/lib.rs
+/workspace/rust-javascript/01-hello/hello/target/rls/debug/deps/hello-35e8144458653042.d: src/lib.rs
 
 src/lib.rs:
--- 01-hello/hello/target/rls/debug/deps/hello-e1b9462500f1fe2a.d
+++ 01-hello/hello/target/rls/debug/deps/hello-e1b9462500f1fe2a.d
@@ -1,5 +1,5 @@
-/workspace/rust-browser/01-hello/hello/target/rls/debug/deps/hello-e1b9462500f1fe2a.rmeta: src/lib.rs
+/workspace/rust-javascript/01-hello/hello/target/rls/debug/deps/hello-e1b9462500f1fe2a.rmeta: src/lib.rs
 
-/workspace/rust-browser/01-hello/hello/target/rls/debug/deps/hello-e1b9462500f1fe2a.d: src/lib.rs
+/workspace/rust-javascript/01-hello/hello/target/rls/debug/deps/hello-e1b9462500f1fe2a.d: src/lib.rs
 
 src/lib.rs:
